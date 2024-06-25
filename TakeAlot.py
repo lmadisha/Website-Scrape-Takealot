1c2b4b559e2b9d0ba6e98c04538e3a8079cd1249
lmadisha023 <lmadisha023@student.wethinkcode.co.za>
2024-06-25 12:23:30 +0200

    finished README.md and TakeAlot.py.

--- TakeAlot.py
+++ TakeAlot.py
@@ -1,5 +1,5 @@
+import os
 from collections import OrderedDict
-
 import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -22,10 +22,10 @@ driver.get("https://www.takealot.com/")
 #<--------------------------------------------- Searching up the product-------------------------------------------------------->
 
 search = driver.find_element(By.XPATH, '//*[@id="shopfront-app"]/header/div/div/div[2]/form/div[1]/div[1]/input')
-name_product = input('Enter a product or service: ')
+name_product = input('Enter a product or service: ').strip().lower()
 search.send_keys(name_product)
 search.send_keys(Keys.ENTER)
-time.sleep(5)
+time.sleep(2)
 
 #<--------------------------------------------- Getting all the products links--------------------------------------------------->
 
@@ -54,28 +54,20 @@ driver1 = webdriver.Chrome(options=chrome_options)
 
 for l in links:
     driver1.get(l)
-    time.sleep(3)
+    time.sleep(1.5)
     name = driver1.find_element(By.CSS_SELECTOR, "div.product-title h1").text
     price = driver1.find_element(By.CSS_SELECTOR, 'div.pdp-module_sidebar-buybox_1m6Sm span.currency.plus.currency-module_currency_29IIm').text
 
-    # price = float((price[2:]).replace(",", ""))
-
-    # descriptions = driver1.find_elements(By.CSS_SELECTOR, 'div.product-description p')
-    # description = []
-    # for x in descriptions:
-    #     description.append(x.text)
-    # description = description[1:]
-
     try:
         table = driver1.find_element(By.CSS_SELECTOR, 'div.product-info table')
-        time.sleep(2.5)
+        time.sleep(0.5)
     except selenium.common.exceptions.NoSuchElementException:
         barcode = 'No barcode'
         product_info = 'No product information yet!!'
     else:
         soup = BeautifulSoup(table.get_attribute('outerHTML'), "lxml")
         table_data = []
-        time.sleep(2.5)
+        time.sleep(0.5)
         for row in soup.find_all('tr'):
             columns = row.find_all('td')
             output_row = []
@@ -103,25 +95,20 @@ for l in links:
 driver1.close()
 
 #<--------------------------------------------- CSV ------------------------------------------------------------------------------>
-# import pandas as pandasForSortingCSV
 
 
 fields = ['Name', 'Price', 'Barcode', 'Link', 'Product Information']
 
-filename = f"{name_product.replace(' ', '')}.csv"
+user_path = os.path.expanduser("~")
+
+if not os.path.exists(f"{user_path}/products"):
+    os.mkdir(f"{user_path}/products")
+
+filename = f"{user_path}/products/{name_product.replace(' ', '-')}.csv"
 
 information_dict = sorted(information, key=lambda x: x['Price'])
-# print(information_dict)
 
 with open(filename, "wt", encoding='utf-8') as file:
     writer = csv.DictWriter(file, fieldnames=fields)
     writer.writeheader()
     writer.writerows(information_dict)
-
-# csvData = pandasForSortingCSV.read_csv(filename)
-# csvData.sort_values(["Price"],
-#                     axis=0,
-#                     ascending=[True],
-#                     inplace=True)
-#
-# print(csvData['Name']['Price'])
